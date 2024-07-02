b5375a4fcce57291dec3d8bd6bd90dd640b12a8e
clavesi <34937562+clavesi@users.noreply.github.com>
2024-07-02 15:51:02 -0500

    shadcn-svelte and supabase github auth

--- src/routes/+page.server.ts
+++ src/routes/+page.server.ts
@@ -1,9 +1,47 @@
+import { getOrCreateUser } from "$lib/auth";
 import { db } from "$lib/db";
+import { UserTable } from "$lib/db/schema.js";
+import { error } from "@sveltejs/kit";
+import { eq } from "drizzle-orm";
+import { zfd } from "zod-form-data";
 
-export async function load() {
-	const users = await db.query.UserTable.findMany();
+export const load = async ({ locals }) => {
+	const userProfile = await getOrCreateUser(locals);
 
 	return {
-		users
+		userProfile
 	};
-}
+};
+
+export const actions = {
+	default: async ({ request, locals }) => {
+		const userProfile = await getOrCreateUser(locals);
+
+		if (!userProfile) {
+			error(401, "You need to be logged in!");
+		}
+
+		const schema = zfd.formData({
+			email: zfd.text(),
+			name: zfd.text(),
+			username: zfd.text()
+		});
+
+		const { data } = schema.safeParse(await request.formData());
+
+		if (!data) {
+			error(400, "Invalid form data");
+		}
+
+		await db
+			.update(UserTable)
+			.set({
+				email: data.email,
+				name: data.name,
+				username: data.username
+			})
+			.where(eq(UserTable.id, userProfile.id));
+
+		return { success: true };
+	}
+};
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,72 @@
 <script lang="ts">
-	export let data;
+	import { enhance } from "$app/forms";
+	import { invalidate } from "$app/navigation";
+	import { Button } from "$lib/components/ui/button";
+	import { Card, CardContent, CardHeader } from "$lib/components/ui/card";
+	import { Input } from "$lib/components/ui/input";
+	import { Label } from "$lib/components/ui/label";
+	import { onMount } from "svelte";
 
-	console.log(data.users);
+	export let data;
+	const { userProfile } = data;
+
+	console.log(userProfile);
+
+	let email = "";
+	let displayName = "";
+	let username = "";
+
+	onMount(() => {
+		if (userProfile) {
+			email = userProfile.email;
+			displayName = userProfile.name;
+			username = userProfile.username;
+		}
+	});
 </script>
 
-<h1 class="text-3xl font-bold">Welcome to SvelteKit</h1>
-<p>Visit <a href="https://kit.svelte.dev">kit.svelte.dev</a> to read the documentation</p>
+{#if userProfile}
+	<Card>
+		<CardHeader>Manage your profile</CardHeader>
+		<CardContent>
+			<form
+				method="post"
+				use:enhance={({ formData }) => {
+					// Happens before submit
+					formData.set("name", displayName);
+					formData.set("username", username);
+					formData.set("email", email);
+					// Happens after submit
+					return ({ result }) => {
+						if (result.type === "success") {
+							invalidate("/");
+							alert("Updated!");
+						} else {
+							alert("Something went wrong!");
+						}
+					};
+				}}
+			>
+				<div>
+					<Label>Email</Label>
+					<Input bind:value={email} />
+				</div>
+				<div>
+					<Label>Display Name</Label>
+					<Input bind:value={displayName} />
+				</div>
+				<div>
+					<Label>Username</Label>
+					<Input bind:value={username} />
+				</div>
+				<div>
+					<Button type="submit">Update</Button>
+				</div>
+			</form>
+		</CardContent>
+	</Card>
+
+	<Button href="/auth/logout">Logout</Button>
+{:else}
+	<Button href="/auth/login">Login to the site</Button>
+{/if}
