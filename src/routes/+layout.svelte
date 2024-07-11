88cb62bf1236a8387d40e0c2a965fc40a7a8366c
clavesi <34937562+clavesi@users.noreply.github.com>
2024-07-11 15:14:37 -0500

    Adjust theme and add dark mode

--- src/routes/+layout.svelte
+++ src/routes/+layout.svelte
@@ -1,19 +1,53 @@
 <script lang="ts">
 	import "../app.css";
+	import { browser } from "$app/environment";
+	import { Sun, Moon } from "lucide-svelte";
 	import { Button } from "$lib/components/ui/button";
+
+	let darkMode = true;
+	function handleThemeSwitch() {
+		darkMode = !darkMode;
+
+		localStorage.setItem("theme", darkMode ? "dark" : "light");
+
+		darkMode
+			? document.documentElement.classList.add("dark")
+			: document.documentElement.classList.remove("dark");
+	}
+	if (browser) {
+		if (
+			localStorage.theme === "dark" ||
+			(!("theme" in localStorage) && window.matchMedia("(prefers-color-scheme: dark)").matches)
+		) {
+			darkMode = true;
+		} else {
+			darkMode = false;
+		}
+	}
 
 	export let data;
 </script>
 
+<svelte:head></svelte:head>
+
 <nav>
-	<div class="flex items-center justify-between bg-green-500 p-4 text-xl text-white">
+	<div class="flex items-center justify-between p-4 mb-2 text-xl border-b">
 		<a href="/" class="text-4xl font-bold">omnilog</a>
 		<div class="flex items-center gap-4">
-			{#if data.username}
-				<Button href="/auth/logout" class="rounded-md">Sign Out</Button>
-				<img src={data.imageUrl} alt="user profile" class="size-12 rounded-full" />
+			{#if darkMode}
+				<Button variant="secondary" on:click={handleThemeSwitch}>
+					<Moon />
+				</Button>
 			{:else}
-				<a href="/auth/login">Login</a>
+				<Button variant="secondary" on:click={handleThemeSwitch}>
+					<Sun />
+				</Button>
+			{/if}
+			{#if data.username}
+				<img src={data.imageUrl} alt="user profile" class="rounded-full size-12" />
+				<Button href="/auth/logout" class="rounded-md">Sign Out</Button>
+			{:else}
+				<Button href="/auth/login" class="rounded-md">Login</Button>
 			{/if}
 		</div>
 	</div>
--- src/routes/auth/login/+page.svelte
+++ src/routes/auth/login/+page.svelte
@@ -6,12 +6,12 @@
 	import { Icons } from "$lib/components/icons";
 </script>
 
-<div class="flex h-[calc(100vh-72px)] w-full items-center justify-center">
-	<Card.Root class="w-full max-w-sm rounded-lg border bg-card text-card-foreground shadow-sm">
+<div class="flex h-[calc(100vh-81px)] w-full items-center justify-center">
+	<Card.Root class="w-full max-w-sm border rounded-lg shadow-sm bg-card text-card-foreground">
 		<Card.Header>
 			<Card.Title class="text-2xl">Login</Card.Title>
 		</Card.Header>
-		<Card.Content class="flex w-full flex-col justify-between gap-2">
+		<Card.Content class="flex flex-col justify-between w-full gap-2">
 			<div class="grid gap-2">
 				<Label for="email">Email</Label>
 				<Input id="email" type="email" placeholder="example@example.com" />
@@ -27,26 +27,18 @@
 					<span class="w-full border-t" />
 				</div>
 				<div class="relative flex justify-center text-xs uppercase">
-					<span class="bg-card px-2 text-muted-foreground"> Or continue with </span>
+					<span class="px-2 bg-card text-muted-foreground"> Or continue with </span>
 				</div>
 			</div>
 
 			<Button href="/auth/login/github">
-				<Icons.github class="mr-2 h-4 w-4" />
+				<Icons.github class="w-4 h-4 mr-2" />
 				Github
 			</Button>
 			<Button disabled class="hover:cursor-not-allowed">
-				<Icons.google class="mr-2 h-4 w-4" />
+				<Icons.google class="w-4 h-4 mr-2" />
 				Google
 			</Button>
-			<!-- <div class="relative">
-				<div class="absolute inset-0 flex items-center">
-					<span class="w-full border-t" />
-				</div>
-				<div class="relative flex justify-center text-xs uppercase">
-					<span class="px-2 bg-card text-muted-foreground"> Or continue with </span>
-				</div>
-			</div> -->
 		</Card.Content>
 	</Card.Root>
 </div>
