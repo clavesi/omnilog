6a406196d2ab54396246fae42cbbcf338e3686c6
clavesi <34937562+clavesi@users.noreply.github.com>
2024-11-26 16:41:11 -0600

    Basic db setup

--- src/lib/server/db/schema.ts
+++ src/lib/server/db/schema.ts
@@ -1,8 +1,8 @@
-import { integer, pgTable, serial, text, timestamp } from 'drizzle-orm/pg-core';
+import { integer, pgTable, serial, text } from 'drizzle-orm/pg-core';
 
 export const usersTable = pgTable('users_table', {
-  id: serial('id').primaryKey(),
-  name: text('name').notNull(),
-  age: integer('age').notNull(),
-  email: text('email').notNull().unique(),
+	id: serial('id').primaryKey(),
+	name: text('name').notNull(),
+	age: integer('age').notNull(),
+	email: text('email').notNull().unique()
 });
--- src/routes/+page.ts
+++
@@ -1,7 +0,0 @@
-	import { db } from '$lib/server/db';
-	import * as schema from '$lib/server/db/schema';
-
-	export const load = async () => {
-		const users = await db.query(schema).user.findMany();
-		return { users };
-	};
