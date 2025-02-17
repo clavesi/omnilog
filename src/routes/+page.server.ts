360cfc1940dc0229d2ff73ea40b74d2746f3eaf5
clavesi <34937562+clavesi@users.noreply.github.com>
2025-02-16 22:12:30 -0600

    Restart

--- src/routes/+page.server.ts
+++
@@ -1,6 +0,0 @@
-import { db } from '$lib/server/db/index';
-
-export async function load() {
-	const users = await db.query.usersTable.findMany();
-	return { users };
-}
--- src/routes/+page.svelte
+++
@@ -1,8 +0,0 @@
-<script lang="ts">
-	import type { PageData } from './$types';
-	export let data: PageData;
-	console.log(data.users);
-</script>
-
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
