1432e6ce7753b8acdcb98066ee91e21df40f7c67
clavesi <34937562+clavesi@users.noreply.github.com>
2025-02-16 22:50:42 -0600

    Basic local Neon database setup

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,6 +1,25 @@
 import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
+import { neon, neonConfig } from '@neondatabase/serverless';
 import { env } from '$env/dynamic/private';
+import ws from 'ws';
+
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
-const client = neon(env.DATABASE_URL);
-export const db = drizzle(client);
+
+let connectionString = env.DATABASE_URL;
+
+// Configure for local development
+if (env.NODE_ENV === 'development') {
+    connectionString = 'postgres://postgres:postgres@db.localtest.me:5432/main';
+    neonConfig.fetchEndpoint = (host) => {
+        const [protocol, port] = host === 'db.localtest.me' ? ['http', 4444] : ['https', 443];
+        return `${protocol}://${host}:${port}/sql`;
+    };
+    const connectionStringUrl = new URL(connectionString);
+    neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'db.localtest.me';
+    neonConfig.wsProxy = (host) => (host === 'db.localtest.me' ? `${host}:4444/v2` : `${host}/v2`);
+}
+neonConfig.webSocketConstructor = ws;
+
+const sql = neon(connectionString);
+
+export const db = drizzle(sql);
