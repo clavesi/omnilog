8a3a04783ad3e9fdc6a9daeb93e17ac3b78dff01
clavesi <34937562+clavesi@users.noreply.github.com>
2025-02-26 15:50:36 -0600

    Remove unused imports on main page

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,8 +1,4 @@
 <script lang="ts">
-	import { Button } from '$lib/components/ui/button';
-	import { authClient } from '$lib/auth-client';
-	import { GithubIcon } from 'lucide-svelte';
-
 	let { data } = $props();
 </script>
 
