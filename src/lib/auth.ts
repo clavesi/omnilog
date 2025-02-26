089d4def3ddbe065f43d21f00d63ea610050d7b8
clavesi <34937562+clavesi@users.noreply.github.com>
2025-02-26 13:42:25 -0600

    Add public/local better auth url  and add secret to auth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,12 +1,16 @@
 import { betterAuth } from 'better-auth';
 import { drizzleAdapter } from 'better-auth/adapters/drizzle';
 import db from '$lib/server/db';
-import { GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET } from '$env/static/private';
+import { BETTER_AUTH_URL_PUBLIC, BETTER_AUTH_URL_LOCAL, GITHUB_CLIENT_ID, GITHUB_CLIENT_SECRET, BETTER_AUTH_SECRET } from '$env/static/private';
+
+const BETTER_AUTH_URL = process.env.NODE_ENV == 'production' ? BETTER_AUTH_URL_PUBLIC : BETTER_AUTH_URL_LOCAL;
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
 		provider: 'pg'
 	}),
+	secret: BETTER_AUTH_SECRET,
+	baseURL: BETTER_AUTH_URL,
 	emailAndPassword: {
 		enabled: false
 	},
