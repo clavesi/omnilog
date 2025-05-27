4d6dec7e5a569df06b53187efe481f4f2189d6b7
clavesi <34937562+clavesi@users.noreply.github.com>
2025-05-27 09:44:48 -0500

    Implement basic BetterAuth social sign in for GitHub

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,29 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+	import { authClient } from '$lib/auth-client';
+	const session = authClient.useSession();
+</script>
+
+<div class="flex h-screen flex-col items-center justify-center gap-2">
+	{#if $session.data}
+		<div>
+			<h1>Welcome back, {$session.data.user.name}!</h1>
+			<button
+				class="rounded bg-red-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+				on:click={() => authClient.signOut()}
+			>
+				Logout
+			</button>
+		</div>
+	{:else}
+		<button
+			class="rounded bg-blue-500 px-4 py-2 font-bold text-white hover:bg-blue-700"
+			on:click={() =>
+				authClient.signIn.social({
+					provider: 'github',
+					callbackURL: '/'
+				})}
+		>
+			Sign In with GitHub
+		</button>
+	{/if}
+</div>
