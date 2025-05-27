4d6fda3f7af8633fed07debbe57eb05b56686a45
clavesi <34937562+clavesi@users.noreply.github.com>
2025-05-27 08:32:06 -0500

    reset

--- src/lib/auth-client.ts
+++
@@ -1,8 +0,0 @@
-import { createAuthClient } from 'better-auth/svelte';
-import { PUBLIC_BETTER_AUTH_URL_PUBLIC, PUBLIC_BETTER_AUTH_URL_LOCAL } from '$env/static/public';
-
-const BETTER_AUTH_URL = process.env.NODE_ENV === 'production' ? PUBLIC_BETTER_AUTH_URL_PUBLIC : PUBLIC_BETTER_AUTH_URL_LOCAL;
-
-export const authClient = createAuthClient({
-	baseURL: BETTER_AUTH_URL
-});
