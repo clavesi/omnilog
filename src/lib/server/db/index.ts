aee7ddbee91ec547212518e239e1c2fcd537c7ac
clavesi <34937562+clavesi@users.noreply.github.com>
2025-06-01 11:20:07 -0500

    Setup local neon instance for development

--- src/lib/server/db/index.ts
+++ src/lib/server/db/index.ts
@@ -1,10 +1,18 @@
-import { drizzle } from 'drizzle-orm/neon-http';
-import { neon } from '@neondatabase/serverless';
-import * as schema from './schema';
 import { env } from '$env/dynamic/private';
+import { neonConfig, Pool } from '@neondatabase/serverless';
+import { drizzle as drizzleWs } from 'drizzle-orm/neon-serverless';
+import * as schema from './schema';
+import ws from 'ws';
 
 if (!env.DATABASE_URL) throw new Error('DATABASE_URL is not set');
 
-const client = neon(env.DATABASE_URL);
+const connectionString = env.DATABASE_URL;
+if (process.env.NODE_ENV === 'development') {
+	const connectionStringUrl = new URL(connectionString);
+	neonConfig.useSecureWebSocket = connectionStringUrl.hostname !== 'db.localtest.me';
+	neonConfig.wsProxy = (host) => (host === 'db.localtest.me' ? `${host}:4444/v2` : `${host}/v2`);
+}
+neonConfig.webSocketConstructor = ws;
 
-export const db = drizzle(client, { schema });
+const pool = new Pool({ connectionString });
+export const db = drizzleWs(pool, { schema });
