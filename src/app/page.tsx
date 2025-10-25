c83f970910e987ba41a10c932ade68108bd68fd1
clavesi <34937562+clavesi@users.noreply.github.com>
2025-10-25 00:35:39 -0500

    Remove eslint and add Biome

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,9 +1,9 @@
 export default function Home() {
-    return (
-        <div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
-            <main className="flex min-h-screen w-full max-w-3xl flex-col bg-white dark:bg-black ">
-                <p>Home</p>
-            </main>
-        </div>
-    );
+	return (
+		<div className="flex min-h-screen bg-zinc-50 font-sans dark:bg-black">
+			<main className="flex min-h-screen w-full max-w-3xl flex-col bg-white dark:bg-black ">
+				<p>Home</p>
+			</main>
+		</div>
+	);
 }
