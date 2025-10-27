f08966b7802890f342300acfce8aec56c8aad626
clavesi <34937562+clavesi@users.noreply.github.com>
2025-10-27 18:10:12 -0500

    pnpm format

--- drizzle.config.ts
+++ drizzle.config.ts
@@ -2,10 +2,10 @@ import "dotenv/config";
 import { defineConfig } from "drizzle-kit";
 
 export default defineConfig({
-    schema: "./src/lib/schema.ts",
-    out: "./supabase/migrations",
-    dialect: "postgresql",
-    dbCredentials: {
-        url: process.env.DATABASE_URL!,
-    },
+	schema: "./src/lib/schema.ts",
+	out: "./supabase/migrations",
+	dialect: "postgresql",
+	dbCredentials: {
+		url: process.env.DATABASE_URL!,
+	},
 });
