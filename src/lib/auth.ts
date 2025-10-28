d3faacf4d496cfd64a892e813cc447f9c7a3cb03
clavesi <34937562+clavesi@users.noreply.github.com>
2025-10-28 00:04:41 -0500

    Basis for betterauth

--- src/lib/auth.ts
+++ src/lib/auth.ts
@@ -1,9 +1,23 @@
+import "dotenv/config";
 import { betterAuth } from "better-auth";
 import { drizzleAdapter } from "better-auth/adapters/drizzle";
 import { db } from "@/lib/db"; // your drizzle instance
 
 export const auth = betterAuth({
 	database: drizzleAdapter(db, {
-		provider: "pg", // or "mysql", "sqlite"
+		provider: "pg",
 	}),
+	emailAndPassword: {
+		enabled: true,
+	},
+	socialProviders: {
+		google: {
+			clientId: process.env.GOOGLE_CLIENT_ID!,
+			clientSecret: process.env.GOOGLE_CLIENT_SECRET!,
+		},
+		github: {
+			clientId: process.env.GITHUB_CLIENT_ID!,
+			clientSecret: process.env.GITHUB_CLIENT_SECRET!,
+		},
+	}
 });
