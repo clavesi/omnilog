02bda63e1717607a790bbd4c773501fefac57b4f
clavesi <34937562+clavesi@users.noreply.github.com>
2025-10-28 01:02:40 -0500

    Test precommit

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
-import { drizzle } from "drizzle-orm/postgres-js";
-import postgres from "postgres";
 import "dotenv/config";
+import postgres from "postgres";
+import { drizzle } from "drizzle-orm/postgres-js";
 
 const connectionString = process.env.DATABASE_URL!;
 
