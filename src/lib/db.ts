a35626b2d5f9cd743b6a3d03bbef17c6a6788e52
clavesi <34937562+clavesi@users.noreply.github.com>
2025-10-28 01:04:59 -0500

    Test again

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -1,6 +1,6 @@
 import "dotenv/config";
-import postgres from "postgres";
 import { drizzle } from "drizzle-orm/postgres-js";
+import postgres from "postgres";
 
 const connectionString = process.env.DATABASE_URL!;
 
